5f67ff384c09f71d12fd3cc1e5d2578c6bd7f19e
srikanth02061988 <148212673+srikanth02061988@users.noreply.github.com>
2023-11-01 09:08:00 +0530

    Update Newcomer.py

--- Newcomer.py
+++ Newcomer.py
@@ -1,1 +1,15 @@
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+import json
 
+filename = "sample.json"
+
+container_name="test"
+constr = ""
+
+blob_service_client = BlobServiceClient.from_connection_string(constr)
+container_client = blob_service_client.get_container_client(container_name)
+blob_client = container_client.get_blob_client(filename)
+streamdownloader = blob_client.download_blob()
+
+fileReader = json.loads(streamdownloader.readall())
+print(fileReader)
